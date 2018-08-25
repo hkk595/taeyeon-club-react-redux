6a345e9ca0939799dcd37e2cbaab1f8ee2a72d09
Kakit Ho <hkk565@gmail.com>
2018-08-26 00:36:21 +0800

    Code restructuring

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import './App.css';
+import { Menu, Input, Image } from 'semantic-ui-react'
+import PostList from '../containers/PostListContainer'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Menu borderless fixed="top">
+          <Menu.Item>
+            <Image src='/image/echowall_logo_500px.png' size='mini' />
+          </Menu.Item>
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Input icon="search" placeholder="Search" />
+            </Menu.Item>
+            <Menu.Item name="login" />
+          </Menu.Menu>
+        </Menu>
+        <PostList />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import configureStore from './store/ConfigureStore';
